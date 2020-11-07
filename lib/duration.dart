5ff1bee65ac601161ffa41e72add86f7a342ccf0
Mirko Rapisarda <mirkorap16@gmail.com>
2020-11-07 11:52:58 +0100

    feat: add duration constants

---
+++ lib/duration.dart
@@ -0,0 +1,16 @@
+library stilo;
+
+abstract class StiloDuration {
+  static const d75 = Duration(microseconds: 75);
+  static const d100 = Duration(microseconds: 100);
+  static const d150 = Duration(microseconds: 150);
+  static const d200 = Duration(microseconds: 200);
+  static const d300 = Duration(microseconds: 300);
+  static const d500 = Duration(microseconds: 500);
+  static const d700 = Duration(microseconds: 700);
+  static const d1000 = Duration(microseconds: 1000);
+  static const d1200 = Duration(microseconds: 1200);
+  static const d1500 = Duration(microseconds: 1500);
+  static const d2000 = Duration(microseconds: 2000);
+  static const d3000 = Duration(microseconds: 3000);
+}
