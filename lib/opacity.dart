73b92934b362fb478f2eab827d279a95b64849c7
Mirko Rapisarda <mirkorap16@gmail.com>
2020-11-07 10:45:06 +0100

    feat: add opacity helper methods

---
+++ lib/opacity.dart
@@ -0,0 +1,25 @@
+library stilo;
+
+import 'package:flutter/material.dart';
+
+abstract class StiloOpacity {
+  static none({@required Widget widget}) {
+    return Opacity(opacity: 0.0, child: widget);
+  }
+
+  static thin({@required Widget widget}) {
+    return Opacity(opacity: 0.25, child: widget);
+  }
+
+  static extraLight({@required Widget widget}) {
+    return Opacity(opacity: 0.5, child: widget);
+  }
+
+  static light({@required Widget widget}) {
+    return Opacity(opacity: 0.75, child: widget);
+  }
+
+  static high({@required Widget widget}) {
+    return Opacity(opacity: 1.0, child: widget);
+  }
+}
