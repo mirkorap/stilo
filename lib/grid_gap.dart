4128a4b8cb4a9c075ea493283621c156d272fc2c
Mirko Rapisarda <mirkorap16@gmail.com>
2020-11-07 10:56:26 +0100

    feat: add grid gap constants

---
+++ lib/grid_gap.dart
@@ -0,0 +1,18 @@
+library stilo;
+
+abstract class StiloGridGap {
+  static const g0 = 0.0;
+  static const g1 = 3.0;
+  static const g2 = 6.0;
+  static const g3 = 9.0;
+  static const g4 = 12.0;
+  static const g5 = 15.0;
+  static const g6 = 18.0;
+  static const g8 = 24.0;
+  static const g10 = 30.0;
+  static const g12 = 36.0;
+  static const g16 = 48.0;
+  static const g20 = 60.0;
+  static const g24 = 72.0;
+  static const g32 = 96.0;
+}
