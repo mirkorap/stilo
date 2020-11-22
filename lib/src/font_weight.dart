06d1eae093a1697da23a20c23d9244912799511e
Mirko Rapisarda <mirkorap16@gmail.com>
2020-11-22 11:08:19 +0100

    docs: add font weight numeric value

--- lib/src/font_weight.dart
+++ lib/src/font_weight.dart
@@ -3,30 +3,30 @@ import 'package:flutter/material.dart';
 /// Defines font weight constants
 /// This class remap Flutter's font weights with a more readble name
 abstract class StiloFontWeight {
-  /// Defines a thin font weight
+  /// Defines a thin font weight (100)
   static const thin = FontWeight.w100;
 
-  /// Defines a extra light font weight
+  /// Defines a extra light font weight (200)
   static const extraLight = FontWeight.w200;
 
-  /// Defines a light font weight
+  /// Defines a light font weight (300)
   static const light = FontWeight.w300;
 
-  /// Defines a regular font weight
+  /// Defines a regular font weight (400)
   static const regular = FontWeight.w400;
 
-  /// Defines a medium font weight
+  /// Defines a medium font weight (500)
   static const medium = FontWeight.w500;
 
-  /// Defines a semi bold font weight
+  /// Defines a semi bold font weight (600)
   static const semiBold = FontWeight.w600;
 
-  /// Defines a bold font weight
+  /// Defines a bold font weight (700)
   static const bold = FontWeight.w700;
 
-  /// Defines a extra bold font weight
+  /// Defines a extra bold font weight (800)
   static const extraBold = FontWeight.w800;
 
-  /// Defines a black font weight
+  /// Defines a black font weight (900)
   static const black = FontWeight.w900;
 }
