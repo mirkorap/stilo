cc634f152716d9de1ad0fe10f8d255aa87cdaad9
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-09 09:57:43 +0200

    refactor!: change spacing constants to construct SizedBox widgets

--- lib/src/spacing.dart
+++ lib/src/spacing.dart
@@ -1,365 +1,545 @@
 import 'package:flutter/material.dart';
+import 'package:stilo/src/config.dart';
 
 /// A class that constructs [SizedBox] widgets based on spacing constants
 abstract class StiloSpacing {
-  static const _s0 = 0.0;
-  static const _s1 = 4.0;
-  static const _s2 = 8.0;
-  static const _s3 = 12.0;
-  static const _s4 = 16.0;
-  static const _s5 = 20.0;
-  static const _s6 = 24.0;
-  static const _s7 = 28.0;
-  static const _s8 = 32.0;
-  static const _s9 = 36.0;
-  static const _s10 = 40.0;
-  static const _s11 = 44.0;
-  static const _s12 = 48.0;
-  static const _s14 = 56.0;
-  static const _s16 = 64.0;
-  static const _s20 = 80.0;
-  static const _s24 = 96.0;
-  static const _s28 = 112.0;
-  static const _s32 = 128.0;
-  static const _s36 = 144.0;
-  static const _s40 = 160.0;
-  static const _s44 = 176.0;
-  static const _s48 = 192.0;
-  static const _s52 = 208.0;
-  static const _s56 = 224.0;
-  static const _s60 = 240.0;
-  static const _s64 = 256.0;
-  static const _s72 = 288.0;
-  static const _s80 = 320.0;
-  static const _s96 = 384.0;
-
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 0.0, height: 0.0
-  static const all0 = SizedBox(width: _s0, height: _s0);
+  static const all0 = SizedBox(
+    width: StiloConfig.space0,
+    height: StiloConfig.space0,
+  );
 
   /// Constructs a [SizedBox] widget with width: 0.0
-  static const horiz0 = SizedBox(width: _s0);
+  static const horiz0 = SizedBox(
+    width: StiloConfig.space0,
+  );
 
   /// Constructs a [SizedBox] widget with height: 0.0
-  static const vert0 = SizedBox(height: _s0);
+  static const vert0 = SizedBox(
+    height: StiloConfig.space0,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 4.0, height: 4.0
-  static const all1 = SizedBox(width: _s1, height: _s1);
+  static const all1 = SizedBox(
+    width: StiloConfig.space1,
+    height: StiloConfig.space1,
+  );
 
   /// Constructs a [SizedBox] widget with width: 4.0
-  static const horiz1 = SizedBox(width: _s1);
+  static const horiz1 = SizedBox(
+    width: StiloConfig.space1,
+  );
 
   /// Constructs a [SizedBox] widget with height: 4.0
-  static const vert1 = SizedBox(height: _s1);
+  static const vert1 = SizedBox(
+    height: StiloConfig.space1,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 8.0, height: 8.0
-  static const all2 = SizedBox(width: _s2, height: _s2);
+  static const all2 = SizedBox(
+    width: StiloConfig.space2,
+    height: StiloConfig.space2,
+  );
 
   /// Constructs a [SizedBox] widget with width: 8.0
-  static const horiz2 = SizedBox(width: _s2);
+  static const horiz2 = SizedBox(
+    width: StiloConfig.space2,
+  );
 
   /// Constructs a [SizedBox] widget with height: 8.0
-  static const vert2 = SizedBox(height: _s2);
+  static const vert2 = SizedBox(
+    height: StiloConfig.space2,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 12.0, height: 12.0
-  static const all3 = SizedBox(width: _s3, height: _s3);
+  static const all3 = SizedBox(
+    width: StiloConfig.space3,
+    height: StiloConfig.space3,
+  );
 
   /// Constructs a [SizedBox] widget with width: 12.0
-  static const horiz3 = SizedBox(width: _s3);
+  static const horiz3 = SizedBox(
+    width: StiloConfig.space3,
+  );
 
   /// Constructs a [SizedBox] widget with height: 12.0
-  static const vert3 = SizedBox(height: _s3);
+  static const vert3 = SizedBox(
+    height: StiloConfig.space3,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 16.0, height: 16.0
-  static const all4 = SizedBox(width: _s4, height: _s4);
+  static const all4 = SizedBox(
+    width: StiloConfig.space4,
+    height: StiloConfig.space4,
+  );
 
   /// Constructs a [SizedBox] widget with width: 16.0
-  static const horiz4 = SizedBox(width: _s4);
+  static const horiz4 = SizedBox(
+    width: StiloConfig.space4,
+  );
 
   /// Constructs a [SizedBox] widget with height: 16.0
-  static const vert4 = SizedBox(height: _s4);
+  static const vert4 = SizedBox(
+    height: StiloConfig.space4,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 20.0, height: 20.0
-  static const all5 = SizedBox(width: _s5, height: _s5);
+  static const all5 = SizedBox(
+    width: StiloConfig.space5,
+    height: StiloConfig.space5,
+  );
 
   /// Constructs a [SizedBox] widget with width: 20.0
-  static const horiz5 = SizedBox(width: _s5);
+  static const horiz5 = SizedBox(
+    width: StiloConfig.space5,
+  );
 
   /// Constructs a [SizedBox] widget with height: 20.0
-  static const vert5 = SizedBox(height: _s5);
+  static const vert5 = SizedBox(
+    height: StiloConfig.space5,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 24.0, height: 24.0
-  static const all6 = SizedBox(width: _s6, height: _s6);
+  static const all6 = SizedBox(
+    width: StiloConfig.space6,
+    height: StiloConfig.space6,
+  );
 
   /// Constructs a [SizedBox] widget with width: 24.0
-  static const horiz6 = SizedBox(width: _s6);
+  static const horiz6 = SizedBox(
+    width: StiloConfig.space6,
+  );
 
   /// Constructs a [SizedBox] widget with height: 24.0
-  static const vert6 = SizedBox(height: _s6);
+  static const vert6 = SizedBox(
+    height: StiloConfig.space6,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 28.0, height: 28.0
-  static const all7 = SizedBox(width: _s7, height: _s7);
+  static const all7 = SizedBox(
+    width: StiloConfig.space7,
+    height: StiloConfig.space7,
+  );
 
   /// Constructs a [SizedBox] widget with width: 28.0
-  static const horiz7 = SizedBox(width: _s7);
+  static const horiz7 = SizedBox(
+    width: StiloConfig.space7,
+  );
 
   /// Constructs a [SizedBox] widget with height: 28.0
-  static const vert7 = SizedBox(height: _s7);
+  static const vert7 = SizedBox(
+    height: StiloConfig.space7,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 32.0, height: 32.0
-  static const all8 = SizedBox(width: _s8, height: _s8);
+  static const all8 = SizedBox(
+    width: StiloConfig.space8,
+    height: StiloConfig.space8,
+  );
 
   /// Constructs a [SizedBox] widget with width: 32.0
-  static const horiz8 = SizedBox(width: _s8);
+  static const horiz8 = SizedBox(
+    width: StiloConfig.space8,
+  );
 
   /// Constructs a [SizedBox] widget with height: 32.0
-  static const vert8 = SizedBox(height: _s8);
+  static const vert8 = SizedBox(
+    height: StiloConfig.space8,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 36.0, height: 36.0
-  static const all9 = SizedBox(width: _s9, height: _s9);
+  static const all9 = SizedBox(
+    width: StiloConfig.space9,
+    height: StiloConfig.space9,
+  );
 
   /// Constructs a [SizedBox] widget with width: 36.0
-  static const horiz9 = SizedBox(width: _s9);
+  static const horiz9 = SizedBox(
+    width: StiloConfig.space9,
+  );
 
   /// Constructs a [SizedBox] widget with height: 36.0
-  static const vert9 = SizedBox(height: _s9);
+  static const vert9 = SizedBox(
+    height: StiloConfig.space9,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 40.0, height: 40.0
-  static const all10 = SizedBox(width: _s10, height: _s10);
+  static const all10 = SizedBox(
+    width: StiloConfig.space10,
+    height: StiloConfig.space10,
+  );
 
   /// Constructs a [SizedBox] widget with width: 40.0
-  static const horiz10 = SizedBox(width: _s10);
+  static const horiz10 = SizedBox(
+    width: StiloConfig.space10,
+  );
 
   /// Constructs a [SizedBox] widget with height: 40.0
-  static const vert10 = SizedBox(height: _s10);
+  static const vert10 = SizedBox(
+    height: StiloConfig.space10,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 44.0, height: 44.0
-  static const all11 = SizedBox(width: _s11, height: _s11);
+  static const all11 = SizedBox(
+    width: StiloConfig.space11,
+    height: StiloConfig.space11,
+  );
 
   /// Constructs a [SizedBox] widget with width: 44.0
-  static const horiz11 = SizedBox(width: _s11);
+  static const horiz11 = SizedBox(
+    width: StiloConfig.space11,
+  );
 
   /// Constructs a [SizedBox] widget with height: 44.0
-  static const vert11 = SizedBox(height: _s11);
+  static const vert11 = SizedBox(
+    height: StiloConfig.space11,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 48.0, height: 48.0
-  static const all12 = SizedBox(width: _s12, height: _s12);
+  static const all12 = SizedBox(
+    width: StiloConfig.space12,
+    height: StiloConfig.space12,
+  );
 
   /// Constructs a [SizedBox] widget with width: 48.0
-  static const horiz12 = SizedBox(width: _s12);
+  static const horiz12 = SizedBox(
+    width: StiloConfig.space12,
+  );
 
   /// Constructs a [SizedBox] widget with height: 48.0
-  static const vert12 = SizedBox(height: _s12);
+  static const vert12 = SizedBox(
+    height: StiloConfig.space12,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 56.0, height: 56.0
-  static const all14 = SizedBox(width: _s14, height: _s14);
+  static const all14 = SizedBox(
+    width: StiloConfig.space14,
+    height: StiloConfig.space14,
+  );
 
   /// Constructs a [SizedBox] widget with width: 56.0
-  static const horiz14 = SizedBox(width: _s14);
+  static const horiz14 = SizedBox(
+    width: StiloConfig.space14,
+  );
 
   /// Constructs a [SizedBox] widget with height: 56.0
-  static const vert14 = SizedBox(height: _s14);
+  static const vert14 = SizedBox(
+    height: StiloConfig.space14,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 64.0, height: 64.0
-  static const all16 = SizedBox(width: _s16, height: _s16);
+  static const all16 = SizedBox(
+    width: StiloConfig.space16,
+    height: StiloConfig.space16,
+  );
 
   /// Constructs a [SizedBox] widget with width: 64.0
-  static const horiz16 = SizedBox(width: _s16);
+  static const horiz16 = SizedBox(
+    width: StiloConfig.space16,
+  );
 
   /// Constructs a [SizedBox] widget with height: 64.0
-  static const vert16 = SizedBox(height: _s16);
+  static const vert16 = SizedBox(
+    height: StiloConfig.space16,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 80.0, height: 80.0
-  static const all20 = SizedBox(width: _s20, height: _s20);
+  static const all20 = SizedBox(
+    width: StiloConfig.space20,
+    height: StiloConfig.space20,
+  );
 
   /// Constructs a [SizedBox] widget with width: 80.0
-  static const horiz20 = SizedBox(width: _s20);
+  static const horiz20 = SizedBox(
+    width: StiloConfig.space20,
+  );
 
   /// Constructs a [SizedBox] widget with height: 80.0
-  static const vert20 = SizedBox(height: _s20);
+  static const vert20 = SizedBox(
+    height: StiloConfig.space20,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 96.0, height: 96.0
-  static const all24 = SizedBox(width: _s24, height: _s24);
+  static const all24 = SizedBox(
+    width: StiloConfig.space24,
+    height: StiloConfig.space24,
+  );
 
   /// Constructs a [SizedBox] widget with width: 96.0
-  static const horiz24 = SizedBox(width: _s24);
+  static const horiz24 = SizedBox(
+    width: StiloConfig.space24,
+  );
 
   /// Constructs a [SizedBox] widget with height: 96.0
-  static const vert24 = SizedBox(height: _s24);
+  static const vert24 = SizedBox(
+    height: StiloConfig.space24,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 112.0, height: 112.0
-  static const all28 = SizedBox(width: _s28, height: _s28);
+  static const all28 = SizedBox(
+    width: StiloConfig.space28,
+    height: StiloConfig.space28,
+  );
 
   /// Constructs a [SizedBox] widget with width: 112.0
-  static const horiz28 = SizedBox(width: _s28);
+  static const horiz28 = SizedBox(
+    width: StiloConfig.space28,
+  );
 
   /// Constructs a [SizedBox] widget with height: 112.0
-  static const vert28 = SizedBox(height: _s28);
+  static const vert28 = SizedBox(
+    height: StiloConfig.space28,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 128.0, height: 128.0
-  static const all32 = SizedBox(width: _s32, height: _s32);
+  static const all32 = SizedBox(
+    width: StiloConfig.space32,
+    height: StiloConfig.space32,
+  );
 
   /// Constructs a [SizedBox] widget with width: 128.0
-  static const horiz32 = SizedBox(width: _s32);
+  static const horiz32 = SizedBox(
+    width: StiloConfig.space32,
+  );
 
   /// Constructs a [SizedBox] widget with height: 128.0
-  static const vert32 = SizedBox(height: _s32);
+  static const vert32 = SizedBox(
+    height: StiloConfig.space32,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 144.0, height: 144.0
-  static const all36 = SizedBox(width: _s36, height: _s36);
+  static const all36 = SizedBox(
+    width: StiloConfig.space36,
+    height: StiloConfig.space36,
+  );
 
   /// Constructs a [SizedBox] widget with width: 144.0
-  static const horiz36 = SizedBox(width: _s36);
+  static const horiz36 = SizedBox(
+    width: StiloConfig.space36,
+  );
 
   /// Constructs a [SizedBox] widget with height: 144.0
-  static const vert36 = SizedBox(height: _s36);
+  static const vert36 = SizedBox(
+    height: StiloConfig.space36,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 160.0, height: 160.0
-  static const all40 = SizedBox(width: _s40, height: _s40);
+  static const all40 = SizedBox(
+    width: StiloConfig.space40,
+    height: StiloConfig.space40,
+  );
 
   /// Constructs a [SizedBox] widget with width: 160.0
-  static const horiz40 = SizedBox(width: _s40);
+  static const horiz40 = SizedBox(
+    width: StiloConfig.space40,
+  );
 
   /// Constructs a [SizedBox] widget with height: 160.0
-  static const vert40 = SizedBox(height: _s40);
+  static const vert40 = SizedBox(
+    height: StiloConfig.space40,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 176.0, height: 176.0
-  static const all44 = SizedBox(width: _s44, height: _s44);
+  static const all44 = SizedBox(
+    width: StiloConfig.space44,
+    height: StiloConfig.space44,
+  );
 
   /// Constructs a [SizedBox] widget with width: 176.0
-  static const horiz44 = SizedBox(width: _s44);
+  static const horiz44 = SizedBox(
+    width: StiloConfig.space44,
+  );
 
   /// Constructs a [SizedBox] widget with height: 176.0
-  static const vert44 = SizedBox(height: _s44);
+  static const vert44 = SizedBox(
+    height: StiloConfig.space44,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 192.0, height: 192.0
-  static const all48 = SizedBox(width: _s48, height: _s48);
+  static const all48 = SizedBox(
+    width: StiloConfig.space48,
+    height: StiloConfig.space48,
+  );
 
   /// Constructs a [SizedBox] widget with width: 192.0
-  static const horiz48 = SizedBox(width: _s48);
+  static const horiz48 = SizedBox(
+    width: StiloConfig.space48,
+  );
 
   /// Constructs a [SizedBox] widget with height: 192.0
-  static const vert48 = SizedBox(height: _s48);
+  static const vert48 = SizedBox(
+    height: StiloConfig.space48,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 208.0, height: 208.0
-  static const all52 = SizedBox(width: _s52, height: _s52);
+  static const all52 = SizedBox(
+    width: StiloConfig.space52,
+    height: StiloConfig.space52,
+  );
 
   /// Constructs a [SizedBox] widget with width: 208.0
-  static const horiz52 = SizedBox(width: _s52);
+  static const horiz52 = SizedBox(
+    width: StiloConfig.space52,
+  );
 
   /// Constructs a [SizedBox] widget with height: 208.0
-  static const vert52 = SizedBox(height: _s52);
+  static const vert52 = SizedBox(
+    height: StiloConfig.space52,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 224.0, height: 224.0
-  static const all56 = SizedBox(width: _s56, height: _s56);
+  static const all56 = SizedBox(
+    width: StiloConfig.space56,
+    height: StiloConfig.space56,
+  );
 
   /// Constructs a [SizedBox] widget with width: 224.0
-  static const horiz56 = SizedBox(width: _s56);
+  static const horiz56 = SizedBox(
+    width: StiloConfig.space56,
+  );
 
   /// Constructs a [SizedBox] widget with height: 224.0
-  static const vert56 = SizedBox(height: _s56);
+  static const vert56 = SizedBox(
+    height: StiloConfig.space56,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 240.0, height: 240.0
-  static const all60 = SizedBox(width: _s60, height: _s60);
+  static const all60 = SizedBox(
+    width: StiloConfig.space60,
+    height: StiloConfig.space60,
+  );
 
   /// Constructs a [SizedBox] widget with width: 240.0
-  static const horiz60 = SizedBox(width: _s60);
+  static const horiz60 = SizedBox(
+    width: StiloConfig.space60,
+  );
 
   /// Constructs a [SizedBox] widget with height: 240.0
-  static const vert60 = SizedBox(height: _s60);
+  static const vert60 = SizedBox(
+    height: StiloConfig.space60,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 256.0, height: 256.0
-  static const all64 = SizedBox(width: _s64, height: _s64);
+  static const all64 = SizedBox(
+    width: StiloConfig.space64,
+    height: StiloConfig.space64,
+  );
 
   /// Constructs a [SizedBox] widget with width: 256.0
-  static const horiz64 = SizedBox(width: _s64);
+  static const horiz64 = SizedBox(
+    width: StiloConfig.space64,
+  );
 
   /// Constructs a [SizedBox] widget with height: 256.0
-  static const vert64 = SizedBox(height: _s64);
+  static const vert64 = SizedBox(
+    height: StiloConfig.space64,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 288.0, height: 288.0
-  static const all72 = SizedBox(width: _s72, height: _s72);
+  static const all72 = SizedBox(
+    width: StiloConfig.space72,
+    height: StiloConfig.space72,
+  );
 
   /// Constructs a [SizedBox] widget with width: 288.0
-  static const horiz72 = SizedBox(width: _s72);
+  static const horiz72 = SizedBox(
+    width: StiloConfig.space72,
+  );
 
   /// Constructs a [SizedBox] widget with height: 288.0
-  static const vert72 = SizedBox(height: _s72);
+  static const vert72 = SizedBox(
+    height: StiloConfig.space72,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 320.0, height: 320.0
-  static const all80 = SizedBox(width: _s80, height: _s80);
+  static const all80 = SizedBox(
+    width: StiloConfig.space80,
+    height: StiloConfig.space80,
+  );
 
   /// Constructs a [SizedBox] widget with width: 320.0
-  static const horiz80 = SizedBox(width: _s80);
+  static const horiz80 = SizedBox(
+    width: StiloConfig.space80,
+  );
 
   /// Constructs a [SizedBox] widget with height: 320.0
-  static const vert80 = SizedBox(height: _s80);
+  static const vert80 = SizedBox(
+    height: StiloConfig.space80,
+  );
 
   /// Constructs a [SizedBox] widget with:
   ///
   /// width: 384.0, height: 384.0
-  static const all96 = SizedBox(width: _s96, height: _s96);
+  static const all96 = SizedBox(
+    width: StiloConfig.space96,
+    height: StiloConfig.space96,
+  );
 
   /// Constructs a [SizedBox] widget with width: 384.0
-  static const horiz96 = SizedBox(width: _s96);
+  static const horiz96 = SizedBox(
+    width: StiloConfig.space96,
+  );
 
   /// Constructs a [SizedBox] widget with height: 384.0
-  static const vert96 = SizedBox(height: _s96);
+  static const vert96 = SizedBox(
+    height: StiloConfig.space96,
+  );
 }
