057ec7b66a4e4ece46793f44654e33ce799c9396
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-15 09:47:21 +0200

    feat!: add new Opacity widget methods

--- lib/src/opacity.dart
+++ lib/src/opacity.dart
@@ -1,29 +1,139 @@
 import 'package:flutter/material.dart';
 
-/// Defines opacity constants
+/// A class that constructs [Opacity] widgets based on opacity constants
 abstract class StiloOpacity {
-  /// Constructs an opacity widget of 0.0 (hide widget)
-  static none({@required Widget widget}) {
-    return Opacity(opacity: 0.0, child: widget);
+  /// Constructs an [Opacity] widget with an opacity of 0%
+  static Opacity opacity0({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.0,
+    );
   }
 
-  /// Constructs an opacity widget of 0.25
-  static thin({@required Widget widget}) {
-    return Opacity(opacity: 0.25, child: widget);
+  /// Constructs an [Opacity] widget with an opacity of 5%
+  static Opacity opacity5({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.05,
+    );
   }
 
-  /// Constructs an opacity widget of 0.5
-  static extraLight({@required Widget widget}) {
-    return Opacity(opacity: 0.5, child: widget);
+  /// Constructs an [Opacity] widget with an opacity of 10%
+  static Opacity opacity10({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.1,
+    );
   }
 
-  /// Constructs an opacity widget of 0.75
-  static light({@required Widget widget}) {
-    return Opacity(opacity: 0.75, child: widget);
+  /// Constructs an [Opacity] widget with an opacity of 20%
+  static Opacity opacity20({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.2,
+    );
   }
 
-  /// Constructs an opacity widget of 1.0 (show widget)
-  static high({@required Widget widget}) {
-    return Opacity(opacity: 1.0, child: widget);
+  /// Constructs an [Opacity] widget with an opacity of 25%
+  static Opacity opacity25({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.25,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 30%
+  static Opacity opacity30({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.3,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 40%
+  static Opacity opacity40({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.4,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 50%
+  static Opacity opacity50({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.5,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 60%
+  static Opacity opacity60({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.6,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 70%
+  static Opacity opacity70({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.7,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 75%
+  static Opacity opacity75({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.75,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 80%
+  static Opacity opacity80({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.8,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 90%
+  static Opacity opacity90({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.9,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 95%
+  static Opacity opacity95({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 0.95,
+    );
+  }
+
+  /// Constructs an [Opacity] widget with an opacity of 100%
+  static Opacity opacity100({Key key, @required Widget child}) {
+    return Opacity(
+      key: key,
+      child: child,
+      opacity: 1.0,
+    );
   }
 }
